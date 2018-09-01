1b9d9cb6b9dd1a2c78673b00a897ea2ce9d0416b
CJ <cj@68167666.com>
2018-09-01 22:39:47 +0800

    增加button组件

--- src/app.js
+++ src/app.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import Buttom from './button'
+import Icon from './icon'
+import  ButtonGroup from './g-button-group'
 
 Vue.component('g-button', Buttom);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
 
 new Vue({
     el: '#app',
+    data: {
+        loading1: false
+    }
 });
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,41 @@
-<template>
-    <button class="g-button">按钮</button>
+<template xmlns="">
+    <button class="g-button" :class="`icon-${iconPosition}`"
+    @click="$emit('click')">
+        <g-icon  class="icon" v-if="icon && !loading" :name="icon"></g-icon>
+        <g-icon  class="icon loading" v-if="loading" name="jiazai"></g-icon>
+        <div class="content">
+            <slot/>
+        </div>
+    </button>
 </template>
 <script>
-    export  default {}
+    export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator: function (value) {
+                    return value === 'left' || value === 'right'
+                }
+            },
+            loading: {
+                type: Boolean,
+                default: false
+            }
+        }
+    }
 </script>
 <style lang="scss">
+    @keyframes spin {
+        0% {
+            transform: rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
     .g-button {
         font-size: var(--font-size);
         height: var(--button-height);
@@ -12,6 +43,10 @@
         border-radius: var(--border-radius);
         border: 1px solid var(--border-color);
         background: var(--button-bg);
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         &:hover {
             border-color: var(--border-color-hover);
         }
@@ -21,6 +56,28 @@
         &:focus {
             outline: none;
         }
+        > .content {
+            order: 2;
+        }
+        > .icon {
+            order: 1;
+            margin-right: .3em;
+        }
+        &.icon-right {
+            > .content {
+                order: 1;
+            }
+            > .icon {
+                order: 2;
+                margin-right: 0;
+                margin-left: .3em;
+            }
+
+        }
+        .loading {
+            animation: spin .5s infinite linear;
+        }
+
     }
 
 </style>
